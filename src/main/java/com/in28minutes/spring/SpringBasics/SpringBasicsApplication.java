40b428342faa4ce8ad5f80f73a4e86604b68ec09
Utkarsh <ignition3951@gmail.com>
2022-01-09 01:43:08 +0530

    Understanding beans management in spring

--- src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsApplication.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsApplication.java
@@ -2,17 +2,26 @@ package com.in28minutes.spring.SpringBasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
+	
+	//What are the beans to be managed-->@Component
+	//What are the dependencies of the bean? --> @Autowired
+	//Where to search for them--> @SpringBootApplication runs the scan in the current 
+	//package and the sub-packages also
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(
-				new QuickSortAlgorithm());
+//		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(
+//				new QuickSortAlgorithm());
+ApplicationContext applicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
+		
+		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearchImpl.binarySearch(new int[] {1,2,3,4},3);
 		
 		System.out.println(result);
-		//SpringApplication.run(SpringBasicsApplication.class, args);
+		
 	}
 
 }

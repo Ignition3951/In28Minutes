36e81347b76cea22498bf09402aefe83aee09e54
Utkarsh <ignition3951@gmail.com>
2022-01-24 21:27:03 +0530

    Understanding @Qualifier annotation (when two classes implement a single interface)

--- src/main/java/com/in28minutes/spring/SpringBasics/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/BinarySearchImpl.java
@@ -1,12 +1,14 @@
 package com.in28minutes.spring.SpringBasics;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 	
 	@Autowired
+	@Qualifier("bubble")
 	private SortAlgorithm sortAlgorithm;
 	
 		
--- src/main/java/com/in28minutes/spring/SpringBasics/BubbleSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.in28minutes.spring.SpringBasics;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	
 	public int[] sort(int[] numbers) {

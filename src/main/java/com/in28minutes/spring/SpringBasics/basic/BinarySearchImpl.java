54de6ad19a691c5c67a3b8e4fbe4c6e815dadd42
Utkarsh <ignition3951@gmail.com>
2022-01-27 20:49:36 +0530

    Understanding PostConstruct and Predestroy annotation

--- src/main/java/com/in28minutes/spring/SpringBasics/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.in28minutes.spring.SpringBasics.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -9,6 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+	
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	@Qualifier("bubble")
@@ -32,6 +39,16 @@ public class BinarySearchImpl {
 		
 		//Search the array
 		return 3;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		LOGGER.info("Post construct method!");		
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		LOGGER.info("Pre Destroy method!");		
 	}
 
 }

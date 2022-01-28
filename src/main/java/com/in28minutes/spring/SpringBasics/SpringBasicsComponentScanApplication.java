70a630e6e96962441aeabf24b750e7450f9e01b5
Utkarsh <ignition3951@gmail.com>
2022-01-28 23:04:22 +0530

    Test branch commit

--- src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsComponentScanApplication.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsComponentScanApplication.java
@@ -2,15 +2,18 @@ package com.in28minutes.spring.SpringBasics;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.SpringBasics.scope.PersonDAO;
 import com.in28minutes.spring.componentscan.ComponentDAO;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
 @ComponentScan("com.in28minutes.spring.componentscan")
 public class SpringBasicsComponentScanApplication {
 	
@@ -19,7 +22,8 @@ public class SpringBasicsComponentScanApplication {
 	public static void main(String[] args) {
 //		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(
 //				new QuickSortAlgorithm());
-		ApplicationContext applicationContext = SpringApplication.run(SpringBasicsComponentScanApplication.class, args);
+		ApplicationContext applicationContext = 
+				new AnnotationConfigApplicationContext(SpringBasicsComponentScanApplication.class);
 
 		ComponentDAO componentDAO = applicationContext.getBean(ComponentDAO.class);
 
--- src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsScopeApplication.java
+++ src/main/java/com/in28minutes/spring/SpringBasics/SpringBasicsScopeApplication.java
@@ -2,13 +2,18 @@ package com.in28minutes.spring.SpringBasics;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.SpringBasics.scope.PersonDAO;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringBasicsScopeApplication {
 	
 	private static Logger LOGGER =  LoggerFactory.getLogger(SpringBasicsScopeApplication.class);
@@ -16,7 +21,8 @@ public class SpringBasicsScopeApplication {
 	public static void main(String[] args) {
 //		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(
 //				new QuickSortAlgorithm());
-		ApplicationContext applicationContext = SpringApplication.run(SpringBasicsScopeApplication.class, args);
+		ApplicationContext applicationContext = 
+new AnnotationConfigApplicationContext(SpringBasicsScopeApplication.class);
 
 		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
 		PersonDAO personDAO1 = applicationContext.getBean(PersonDAO.class);
